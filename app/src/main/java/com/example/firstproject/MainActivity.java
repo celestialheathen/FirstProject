a6e56a1da119a0f26975a76cedfecf4dcd406ecf
celestialheathen <jamesjcyu@gmail.com>
2021-07-29 17:21:10 -0400

    added image view and checkboxes

--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -6,31 +6,71 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text;
-    Button button;
+    EditText name;
+    Button ok;
+    TextView result;
+    String userName;
+    ImageView image;
+    CheckBox male;
+    CheckBox female;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text = findViewById(R.id.textexample);
-        button = findViewById(R.id.domagic);
+        name = findViewById(R.id.editTextName);
+        ok = findViewById(R.id.buttonOk);
+        result = findViewById(R.id.textViewResult);
+        image = findViewById(R.id.imageExample);
 
-        button.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                  button.setBackgroundColor(Color.YELLOW);
-                  text.setText("I have done my magic.");
-                  text.setVisibility(View.INVISIBLE);
-              }
-          });
+        male = findViewById(R.id.checkBoxMale);
+        female = findViewById(R.id.checkBoxfemale);
 
-        text.setTextColor(Color.BLACK);
-        text.setText("This is another example!!");
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (male.isChecked())
+                {
+                    result.setText("You are male.");
+                    female.setChecked(false);
+                }
+                else
+                {
+                    result.setText("What is your gender?");
+                }
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (female.isChecked())
+                {
+                    result.setText("Female!!");
+                    male.setChecked(false);
+                }
+                else
+                {
+                    result.setText("What is your gender??");
+                }
+            }
+        });
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                image.setImageResource(R.drawable.pic1);
+            }
+        });
+
     }
 }
